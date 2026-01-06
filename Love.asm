b69ec75860139991b767601b3e34ba70fdf556ac
Miranda Nigel <adamantite16@gmail.com>
2026-01-06 07:47:53 +0100

    I love you

---
+++ Love.asm
@@ -0,0 +1,31 @@
+option casemap:none
+
+; ---- Libraries ----
+includelib kernel32.lib
+includelib user32.lib
+
+; ---- Function Prototypes ----
+ExitProcess PROTO :QWORD
+MessageBoxA PROTO :QWORD, :QWORD, :QWORD, :QWORD
+
+; ---- Data Section ----
+.data
+    msg     db "I love u ", 0
+    title   db "Message", 0
+
+; ---- Code Section ----
+.code
+main PROC
+    sub rsp, 28h        ; shadow space + stack alignment (IMPORTANT)
+
+    xor rcx, rcx        ; hWnd = NULL
+    lea rdx, msg        ; lpText
+    lea r8, title       ; lpCaption
+    xor r9, r9          ; MB_OK
+    call MessageBoxA
+
+    xor rcx, rcx
+    call ExitProcess
+
+main ENDP
+END
