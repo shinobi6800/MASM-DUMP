a489f35c1a5eeb3a0703ceb275246dbac80c6670
Miranda Nigel <adamantite16@gmail.com>
2026-01-06 07:59:43 +0100

    lock in

--- process_enum.asm
+++ process_enum.asm
@@ -5,3 +5,14 @@ option casemap:none
 ; =========================
 includelib kernel32.lib
 includelib user32.lib
+
+; =========================
+; Prototypes
+; =========================
+ExitProcess PROTO :QWORD
+GetStdHandle PROTO :QWORD
+WriteConsoleA PROTO :QWORD, :QWORD, :QWORD, :QWORD, :QWORD
+
+CreateToolhelp32Snapshot PROTO :QWORD, :QWORD
+Process32First PROTO :QWORD, :QWORD
+Process32Next PROTO :QWORD, :QWORD
